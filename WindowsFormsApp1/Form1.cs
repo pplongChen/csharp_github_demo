cc59c3e4875e2af63cf808483f9c5d4bd7f2ae01
pplong <pplong@gmail.com>
2022-10-08 21:25:37 +0800

    完成第二個需求，跳出gogogo

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -21,5 +21,10 @@ namespace WindowsFormsApp1
         {
             MessageBox.Show("hello world");
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("gogogo");
+        }
     }
 }
