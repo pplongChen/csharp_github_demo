da5b9511bede8d011c17132139b41252be526391
pplong <pplong@gmail.com>
2022-10-08 21:40:35 +0800

    針對研發長需求修改，重新撰寫程式，英文變中文

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -19,12 +19,13 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("hello world");
+            MessageBox.Show("今天我們講中文");
+            MessageBox.Show("大家好");
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("gogogo");
+            MessageBox.Show("衝阿");
         }
     }
 }
